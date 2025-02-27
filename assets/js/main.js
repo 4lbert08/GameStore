2cb5b4e35d38666efe2769883ae3d59c40fcb89b
Alberto José Rodríguez Ruano <alberto.rodriguez.2708@gmail.com>
2025-02-27 18:03:09 +0000

    HandleFormSubmission implemented in all forms.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,19 @@
 import { loadHTMLAndExecuteScripts } from "./modules/includeHTMLRecursive.js";
 import { initializeGalleries } from "./modules/loadGallery.js";
+import { handleFormSubmission } from "./modules/formHandler.js";
+
+console.log("Módulos importados correctamente.");
 
 document.addEventListener("DOMContentLoaded", async () => {
     console.log("Página cargada, inicializando scripts...");
 
-    const page = document.body.dataset.page; // Usa `data-page` en el body de cada HTML
+    const page = document.body.dataset.page;
 
     switch (page) {
         case "home":
+            console.log("Ejecutando script del home...");
             await loadHTMLAndExecuteScripts("#main_header", "../partials/header.html");
             await loadHTMLAndExecuteScripts("#footer", "../partials/footer.html");
-
             await loadHTMLAndExecuteScripts("#gallery1", "../partials/galleryOfGames.html");
             await loadHTMLAndExecuteScripts("#gallery2", "../partials/galleryOfGames.html");
             await loadHTMLAndExecuteScripts("#gallery3", "../partials/galleryOfGames.html");
@@ -18,18 +21,35 @@ document.addEventListener("DOMContentLoaded", async () => {
             break;
 
         case "aboutUs":
+            console.log("Ejecutando script del aboutUs...");
             await loadHTMLAndExecuteScripts("#main_header", "../partials/header.html");
             break;
 
         case "userConfiguration":
+            console.log("Ejecutando script del userConfiguration...");
             await loadHTMLAndExecuteScripts("#main_header", "../partials/header.html");
             await loadHTMLAndExecuteScripts("#footer", "../partials/footer.html");
-
             await loadHTMLAndExecuteScripts("#userMenuButton", "../partials/userMenuButton.html");
             break;
 
-        case "contact":
-            import("./modules/contactPage.js").then(module => module.initContactForm());
+        case "login":
+            console.log("Ejecutando script de login...");
+            handleFormSubmission("#loginForm", "userConfiguration.html");
+            break;
+
+        case "signUp":
+            console.log("Ejecutando script del signUp...");
+            handleFormSubmission("#signUpForm", "userConfiguration.html");
+            break;
+
+        case "lostPassword":
+            console.log("Ejecutando script de lostPassword...");
+            handleFormSubmission("#lostPasswordForm", "OTPVerification.html");
+            break;
+
+        case "OTPVerification":
+            console.log("Ejecutando script de OTPVerification...");
+            handleFormSubmission("#OTPForm", "ResetPassword.html");
             break;
 
         default:
