6d9890b60a4cb75db320f11086d366a7da5df842
Juanelboi <juangarciaboissier@gmail.com>
2025-03-11 18:36:48 +0000

    fix: passwordHandler removed from login added to forgotPassword Also signUp corrected

--- assets/js/modules/viewsScripts/forgotPassword.js
+++ assets/js/modules/viewsScripts/forgotPassword.js
@@ -1,5 +1,7 @@
 import {handleFormSubmission} from "../functions/formHandler.js";
+import {passwordHandler} from "../functions/passwordHandler.js";
 console.log("Funciones cargadas correctamente.");
 
 console.log("Ejecutando script del forgotPassword...");
+passwordHandler("#forgotPassword__Form")
 handleFormSubmission("#forgotPassword__Form", "OTPVerification.html");
--- assets/js/modules/viewsScripts/login.js
+++ assets/js/modules/viewsScripts/login.js
@@ -1,7 +1,6 @@
 import {handleFormSubmission} from "../functions/formHandler.js";
-import {passwordHandler} from "../functions/passwordHandler.js";
 console.log("Funciones cargadas correctamente.");
 
 console.log("Ejecutando script del logIn...");
-passwordHandler()
+
 handleFormSubmission("#loginDiv__Form", "userSettings.html");
